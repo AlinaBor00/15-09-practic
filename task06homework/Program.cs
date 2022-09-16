d302cfad211617b2eca56085112fd55a3b7a4c39
Alina <alinaborisova321@gmail.com>
2022-09-16 21:05:48 +0700

    task06homework

---
+++ task06homework/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным
+// (делится ли оно на 2 без остатка)
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+     Console.WriteLine("No");
+}
