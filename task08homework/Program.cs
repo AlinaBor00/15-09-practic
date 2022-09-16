ed39c71e0b8adacdf79637a46a5419619bf80c9b
Alina <alinaborisova321@gmail.com>
2022-09-16 21:42:58 +0700

    task08homework

---
+++ task08homework/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите целое число >=1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int counter = 1; counter <= num; counter++)
+     if (counter % 2 == 0)
+     Console.WriteLine($"{counter} ");
+     
+
