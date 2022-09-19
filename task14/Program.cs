fbea078a0945adfe81574ac3182b8205bef87d0d
Alina <alinaborisova321@gmail.com>
2022-09-19 16:30:53 +0700

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+//14 -> нет
+//46 -> нет
+//161 -> да
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool IsAliquot7and23(int arg)
+{
+    return arg % 7 == 0 && arg % 23 == 0;
+}
+bool result =  IsAliquot7and23(num);
+if (result) Console.WriteLine("Yes");
+else Console.WriteLine("No");
