47d91691f01d0ba90fe7f9e11729fc7c28f19a11
Alina <alinaborisova321@gmail.com>
2022-09-22 20:22:57 +0700

    task17

---
+++ task17/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая на вход принимает координаты точки X,Y , прричем они не равны 0 и выдает номер 
+//четверти плоскости, в которой находится эта точка
+
+
+Console.WriteLine("Введите координаты точки");
+Console.Write("X: ");
+int xC = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yC = Convert.ToInt32(Console.ReadLine());
+
+int Quarter(int x, int y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
+    return 0;
+}
+
+int quarter = Quarter(xC, yC);
+Console.WriteLine(quarter == 0 ? "Введены некорректные координаты" : quarter);
