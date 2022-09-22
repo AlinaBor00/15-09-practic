311086e5b67ca1c4ac06fa04bb260a8d8d2e4379
Alina <alinaborisova321@gmail.com>
2022-09-22 20:45:02 +0700

    task18

---
+++ task18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных
+ //координат точек в этой четверти (х и у)
+
+Console.WriteLine("Введите четверть: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+ 
+void Coordinates(int n)
+{
+    if(n == 1) Console.WriteLine("x > 0, y > 0");
+    else if(n == 2) Console.WriteLine("x < 0, y > 0");
+    else if(n == 3) Console.WriteLine("x < 0, y < 0");
+    else if(n == 4) Console.WriteLine("x > 0, y < 0");
+    else Console.WriteLine("Введены некорректные данные ");
+}
+
+ Coordinates(quarter);
+ 
