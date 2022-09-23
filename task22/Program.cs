a0887667abaacd3445808d5de218ec8898b67e9d
Alina <alinaborisova321@gmail.com>
2022-09-23 11:58:39 +0700

    task22

---
+++ task22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Square(int number)
+{
+    int counter = 1;
+    while (counter <= number)
+    {
+        Console.WriteLine($"|{counter, 5}|  {counter * counter, 5} |");
+        counter++;
+    }
+}
+
+if (num > 0) Square(num);
+else Console.WriteLine("Введено некорректное число");
