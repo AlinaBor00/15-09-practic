d6d903e6463f045f7f43564ea902508a83725a24
Alina <alinaborisova321@gmail.com>
2022-09-24 11:57:31 +0700

    task19homework1

--- task19homework/Program.cs
+++ task19homework/Program.cs
@@ -6,11 +6,6 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int num1 = number / 10000;
-int num2 = number % 10000 / 1000;
-int num4 = number % 100 / 10;
-int num5 = number % 10;
-
 
 if (number >= 10000 && number < 100000) Palindrome(number);
 else Console.WriteLine("Введено некорректное число");
@@ -18,6 +13,10 @@ else Console.WriteLine("Введено некорректное число");
 
 void Palindrome(int num)
 { 
+    int num1 = num / 10000;
+    int num2 = num % 10000 / 1000;
+    int num4 = num % 100 / 10;
+    int num5 = num % 10;
     if(num1 == num5) 
     { 
         if(num2 == num4)
