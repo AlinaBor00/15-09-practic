9a194ce6033c3cd96ba7fdadb8ec969a75283ac6
Alina <alinaborisova321@gmail.com>
2022-09-26 20:13:05 +0700

    task30

---
+++ task30/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+//var res = new int[8]; используем var только когда присваиваем определенный тип (int и тд.)
+
+int[] array = new int[8];
+
+void Array(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Array(array);
+PrintArray(array);
