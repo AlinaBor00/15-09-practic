b0dcb2405a7568628f76a46326d1616df20cea71
Alina <alinaborisova321@gmail.com>
2022-09-26 19:37:22 +0700

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число N и 
+//выдает произведение чисел от 1 до N
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Multipl(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+
+if(number > 0)
+{
+    int multipl = Multipl(number);
+    Console.WriteLine(($"Произведение чисел от 1 до {number} = {multipl}"));
+}
+else Console.WriteLine("Введено некорректное значение");
