85ce557296335ef12f96248de90f50dd3140b0de
Alina <alinaborisova321@gmail.com>
2022-09-27 20:45:16 +0700

    task25homework

---
+++ task25homework/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Exponen(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+
+if (number2 >= 0)
+{
+    int exponen = Exponen(number1, number2);
+    Console.WriteLine($"Число {number1} в степени {number2} => {exponen}");
+}
+else Console.WriteLine("Введите корректное второе число");
