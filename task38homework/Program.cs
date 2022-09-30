3f6b5420bf54f6ffa4a775d245e232380aa04043
Alina <alinaborisova321@gmail.com>
2022-09-30 13:00:28 +0700

    task38hw

--- task38homework/Program.cs
+++ task38homework/Program.cs
@@ -27,16 +27,12 @@ void PrintArray(double[] array)
 double DiffMaxMinNum(double[] array)
 {
     double diff = default;
-    double max = default;
-    double min = default;
+    double max = array[0];
+    double min = array[0];
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] > max) 
-        {
-            max = array[i]; 
-            min = array[i];
-        }
-        else if(array[i] < min) min = array[i];
+        if(array[i] > max)  max = array[i]; 
+        if(array[i] < min) min = array[i];
 
         diff = max - min; 
     }
